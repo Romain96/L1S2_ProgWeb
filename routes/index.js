e7f5c62d3c3170d61c88c2b1358cd1424f836e22
Romain96 <romain.perrin@etu.unistra.fr>
2015-05-04 12:34:22 +0200

    Configuration réussie de la balise form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var tweets = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Minitwr' });
+  res.render('index', { title: 'Minitwr', tweets: tweets });
+});
+
+router.post('/', function(req, res, next) {
+    tweets.unshift(req.body.tweet);
+    res.redirect('/')
 });
 
 module.exports = router;
