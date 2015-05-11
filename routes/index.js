056e662e4dba371342e885f1a074c56cfbe80fd9
Romain96 <romain.perrin@etu.unistra.fr>
2015-05-11 13:04:07 +0200

    Ajout des styles par Romain et du JS par Florian

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-var twits = [];
+var twits = [10];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Twit\'Heure', twits: twits });
+  res.render('index', { title: 'Mini Twit\'Heure', twits: twits});
 });
 
 router.post('/', function(req, res, next) {
-    twits.unshift(req.body.twit);
+	var twit = req.body.twit;
+	var name = req.body.name;
+	if (name!=""&&twit!=""){
+		var now = new Date;	
+		var day = now.getDate();
+		var month = now.getMonth()+1;
+		var year = now.getFullYear();
+		var date = day+'/'+month+'/'+year;
+		var hours = now.getHours();
+		var min = now.getMinutes();
+		var hour = hours+'h'+min;
+		twits.unshift({twit:twit,name:name,date:date,hour:hour});
+	}
+	if (twits.length==11)
+		twits.pop();
     res.redirect('/')
 });
 
